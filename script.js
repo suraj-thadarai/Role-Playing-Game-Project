5acb60090976c4a5bb5c5d703307e9edb89b55c2
suraj-thadarai <42627217+suraj-thadarai@users.noreply.github.com>
2023-12-28 10:37:00 +0545

    Update script.js
    defeatMonster() function updated

--- script.js
+++ script.js
@@ -198,10 +198,14 @@ function sellWeapon() {
     }
 }
 
-function defeatMonster(){
+function defeatMonster() {
     gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+
 }
 
-function lose(){
-  
+function lose() {
+
 }
