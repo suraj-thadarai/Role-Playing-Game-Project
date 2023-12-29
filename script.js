d134cca73608c6fd9b53c1f56fd7635c517c1bec
suraj-thadarai <42627217+suraj-thadarai@users.noreply.github.com>
2023-12-29 17:47:07 +0545

    Update script.js
    program completed

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const locations = [
     {
         name: "kill monster",
         "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, goTown],
+        "button functions": [goTown, goTown, easterEgg],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
     {
@@ -277,6 +277,25 @@ function easterEgg() {
 
 function pick(guess) {
     let numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11))
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n"
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + '\n';
+    }
+    if (numbers.indexOf(guess) !== -1) {
+        text.innerText += "Right! You win 20 gold!";
+        gold += 20;
+        goldText.innerText = gold;
+    } else {
+        text.innerText += "Wrong! You lose 10 health!";
+        health -= 10;
+        if (health <= 0) {
+            lose();
+        }
+        healthText.innerText = health;
+    }
 }
 
 function pickTwo() {
