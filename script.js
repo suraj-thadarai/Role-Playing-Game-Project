a139f347bfe2ba825d9e8433d8eb41df1f74e3ac
suraj-thadarai <42627217+suraj-thadarai@users.noreply.github.com>
2023-12-29 17:56:24 +0545

    error corrected
    error corrected

--- script.js
+++ script.js
@@ -188,8 +188,9 @@ function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
     monsterStats.style.display = "block";
-    monsterName.innerText = monsters[fighting].name;
+    monsterName.innerText = monsters[fighting].name;    
     monsterHealthText.innerText = monsterHealth;
+    
 }
 
 function attack() {
